7d74ea33b80dbc8ef6488b0350aedd013e0f511c
Halina <gella93@gmail.com>
2017-07-28 08:59:47 +0200

    adding and updating the files

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 var main = function() {
     $('.icon-menu').click(function() {
     	$('.overlay').css('width','100%');
+    	$('.icon-menu').css('visibility','hidden');
     });
 
     $('.btn-close').click(function() {
     	$('.overlay').css('width','0%');
+    	$('.icon-menu').css('visibility','visible');
     })
 };
 
