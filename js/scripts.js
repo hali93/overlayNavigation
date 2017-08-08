1783a1790c4191a118290f7687d7c5734a6c823a
Halina <gella93@gmail.com>
2017-08-08 11:56:17 +0200

    modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
 var main = function() {
-    $('#menu-open').click(function() {
-    	$('.overlay').css('width','100%');
-    	$('#menu-open').css('visibility','hidden');
-    	$('#menu-close').css('visibility','visible');
+    $('.menu-button').click(function() {
+    	$('.overlay').toggleClass('open');
     });
-
-    $('#menu-close').click(function() {
-    	$('.overlay').css('width','0%');
-    	$('#menu-open').css('visibility','visible'); 
-    	$('#menu-close').css('visibility','hidden');
-    })
 };
 
 $(main);
